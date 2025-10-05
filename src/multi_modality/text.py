438f75774f6faacd31c8f0e18a089e5cd54617a0
hibana2077 <hibana2077@gmail.com>
2025-10-05 12:16:30 +0800

    feat: Add comprehensive labels and descriptions for medicinal materials in text.py

---
+++ src/multi_modality/text.py
@@ -0,0 +1,28 @@
+ALL_LABELS = ['Alpiniae_Katsumadai_Semen', 'Amomi_Fructus', 'Amomi_Fructus_Rotundus', 'Armeniacae_Semen_Amarum', 'Chaenomelis_Fructus', 'Corni_Fructus', 'Crataegi_Fructus', 'Foeniculi_Fructus', 'Forsythiae_Fructus', 'Gardeniae_Fructus', 'Kochiae_Fructus', 'Lycii_Fructus', 'Mume_Fructus', 'Persicae_Semen', 'Psoraleae_Fructus', 'Rosae_Laevigatae_Frucyus', 'Rubi_Fructus', 'Schisandrae_Chinensis_Fructus', 'Toosendan_Fructus', 'Trichosanthis_Pericarpoium']
+# 產地, 品質, 性味, 功效
+# Production Regions, Quality Requirements, Properties, Functions, Category
+LABELS_2_TEXT = {
+    'Alpiniae_Katsumadai_Semen': [
+        'Primarily produced in the Chinese provinces of Guangdong, Guangxi, Hainan.',
+        'Superior medicinal material has full seeds, firm, semi-spherical, and potent aroma and taste.',
+        'Acrid; warm.',
+        'Dries dampness, relieves intention, promoting qi-flowing, warms center, fortifies the stomach, digests food. Splenic and gastric stagnation of cold-damp, chest impediment of damp-warm, lost appetite, cold-dampness and vomiting, cheats and abdominal impediment, indigestion.',
+        'Fruits and seeds'
+    ],
+    'Amomi_Fructus': [
+        'Primarily produced in the Chinese provinces of Guangdong, Guangxi. Imported sha ren is produced in Vietnam, Thailand, Myanmar, and Indonesia.',
+        'Superior medicinal material is large, firm, full, with potent aroma.',
+        'Acrid; warm.',
+        'Transforms dampness, increases appetite, warms spleen, relieves diarrhea, regulates qi, calms fetus. Apply to retention of dampness in the middle, abdominal fullness and anorexia, deficiency and cold spleen and stomach, vomit and diarrhea, pernicious vomiting, restless fetal movements.',
+        'Fruits and seeds'
+    ],
+    'Amomi_Fructus_Rotundus': [
+        'Primarily produced in Thailand, Cambodia, and Myanmar.',
+        'Superior medicinal material is large, full, firm with a potent aromatic odor.',
+        'Acrid; warm.',
+        'Transforms dampness, disperses of local distention, moves qi, warms the center, opens stomach, disperses food. Apply to retention of damp leading to blockage of middle, anorexia, onset of damp thermosis, and fullness due to chest distress, gastrofrigid vomiting, chest and abdominal swelling pain, indigestion.',
+        'Fruits and seeds'
+    ],
+
+
+}
