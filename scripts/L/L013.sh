2d7a84449a21c6b238ba450506b92b87a318437a
hibana2077 <hibana2077@gmail.com>
2025-10-05 15:19:19 +0800

    feat: Add L002.sh to L014.sh scripts for model training with specified configurations

---
+++ scripts/L/L013.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1            
+#PBS -l ncpus=12            
+#PBS -l mem=12GB           
+#PBS -l walltime=00:50:00  
+#PBS -l wd                  
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+# module load python3/3.10.4
+
+source /scratch/rp06/sl5952/Mica/.venv/bin/activate
+
+cd ../..
+
+python3 src/run.py --train_dir dataset/Dataset_1_Cleaned --test_dir dataset/Dataset_2_Cleaned --output_dir output/L013 --seed 42 --model_name swin_tiny_patch4_window7_224.ms_in22k_ft_in1k --no_amp --no_tqdm --epochs 200 >> L013.log 2>&1
