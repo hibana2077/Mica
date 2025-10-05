0fba5ac89eccc471573feb3320efd01085cb4489
hibana2077 <hibana2077@gmail.com>
2025-10-05 09:53:52 +0800

    feat: Redirect output of training script to log file for better tracking

--- scripts/T/T001.sh
+++ scripts/T/T001.sh
@@ -15,4 +15,4 @@ source /scratch/rp06/sl5952/Mica/.venv/bin/activate
 
 cd ../..
 
-python3 src/run.py --train_dir dataset/Dataset_1_Cleaned --test_dir dataset/Dataset_2_Cleaned --output_dir output/T001 --seed 42 --model_name deit_tiny_distilled_patch16_224.fb_in1k --no_amp --no_tqdm
+python3 src/run.py --train_dir dataset/Dataset_1_Cleaned --test_dir dataset/Dataset_2_Cleaned --output_dir output/T001 --seed 42 --model_name deit_tiny_distilled_patch16_224.fb_in1k --no_amp --no_tqdm >> output/T001/log.txt 2>&1
